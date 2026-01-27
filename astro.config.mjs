95ccaba57e1bfb0ee3ff091ad06dedea8f8cad50
Derrick <derrickscott.ux@gmail.com>
2026-01-27 00:37:59 -0500

    Initial Upload

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({});
+
+base: "/portfolio"
+
+site: "https://derrickscottux-collab.github.io"
