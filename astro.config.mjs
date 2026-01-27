c48c1f90a44c0dd19e00612c2d9d4d41eeab93df
Derrick <derrickscott.ux@gmail.com>
2026-01-27 01:02:23 -0500

    Syntax fix and workflow name update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-site: "https://derrickscottux-collab.github.io"
-base: "/"
+site: "https://derrickscottux-collab.github.io",
+base: "/",
 });
